1c59ef4b3c4ffbfdf999b2e37a41f2eb407179a9
Anahit Sargsyan <anahit.sargsyan@dowjones.com>
2020-03-11 19:44:51 +0400

    Added methods of DBL, need to add reverse yet!

--- data_structures/doubly_linked_list.js
+++ data_structures/doubly_linked_list.js
@@ -66,7 +66,67 @@ class DoublyLinkedList {
     }
 
     get(index) {
-        
+        if(index < 0 && index >= this.length) return undefined;
+
+        if(index <= this.length / 2) {
+            const midLimit = this.length/2;
+            let current = this.head;
+            for(let i = 1; i < midLimit; i++) {
+                current = current.next;
+                if(i === index) {
+                    return current;
+                }
+            }
+        } else {
+            const midStart = this.length/2;
+            let current = this.tail;
+            for(let i = this.length - 1; i > midStart; i--) {
+                console.log(i);
+                current = current.prev;
+                if(i === index) {
+                    return current;
+                }
+            }
+        }
+    }
+
+    set(index, value) {
+        const item = this.get(index);
+        if(!item) return false;
+        item.value = value;
+        return true;
+    }
+
+    insert(index, value) {
+        if(index > this.length || index < 0) return false;
+        if(index === this.length) return !!this.push(value);
+        if(index === 0) return !!this.unshift(value);
+
+        const newNode = new Node(value);
+        const currentItem = this.get(index);
+        const prevItem = currentItem.prev;
+
+        prevItem.next = newNode;
+        newNode.prev = prevItem;
+        newNode.next = currentItem;
+        currentItem.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+
+        const beforeEl = this.get(index - 1);
+        const removed = beforeEl.next;
+        const afterEl = removed.next;
+        beforeEl.next = afterEl;
+        afterEl.prev = beforeEl;
+        this.length--;
+
+        return removed;
     }
 }
 
@@ -82,5 +142,18 @@ let list = new DoublyLinkedList();
 list.push(6);
 list.push(2);
 list.push(5);
+list.push(4);
+list.push(7);
+list.push(8);
 list.unshift(12);
-console.log(list);
+list.get(2);
+list.set(2, 12);
+list.insert(3, 33);
+list.remove(3);
+
+console.log(list.head.next.next);
+
+// Insert O(1)
+// remove O(1)
+// Access O(n)
+// Search O(n) but O(n/2)
